6e38a825bfc6c42c5b4ba7a4a1e2ff0b54cca6f6
snwfl <118354319+snwfl@users.noreply.github.com>
2022-11-16 13:26:47 +0200

    Update streamlit_app.py
    Filtering based on selection

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,12 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # pandas demo
-df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-df = df.set_index('Fruit')
+my_fuit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+my_fuit_list = my_fuit_list.set_index('Fruit')
 
 # lets'put a pick list here so they can pick the fuit they want to include
-streamlit.multiselect("Pick some fruits:", list(df.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(df.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fuit_list.loc[fruits_selected]
 
 # display the table on the page
-streamlit.dataframe(df)
+streamlit.dataframe(fruits_to_show)
