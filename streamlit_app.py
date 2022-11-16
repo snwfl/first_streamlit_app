bf30a09a37834c2ff14c966382a0fb55b6cf50e5
snwfl <118354319+snwfl@users.noreply.github.com>
2022-11-16 13:23:25 +0200

    Update streamlit_app.py
    Setting new index on the `Fruit` column

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # pandas demo
 df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+df = df.set_index('Fruit')
 
 # lets'put a pick list here so they can pick the fuit they want to include
 streamlit.multiselect("Pick some fruits:", list(df.index))
