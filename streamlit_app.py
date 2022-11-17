b6c3f86ab75d3c52fa0ff7a02476a17f2c6bbc86
snwfl <118354319+snwfl@users.noreply.github.com>
2022-11-17 11:18:15 +0200

    Update streamlit_app.py
    Added fruit selector

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
@@ -33,4 +35,5 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
 
