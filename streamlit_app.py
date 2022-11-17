a645fb39f35cf6abef4a389fff301fee861d1875
snwfl <118354319+snwfl@users.noreply.github.com>
2022-11-17 11:16:39 +0200

    Update streamlit_app.py
    No more dummy json prints

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,6 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
